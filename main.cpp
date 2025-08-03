77a3ae5853c1bdd41eac755b91bc4779e46c164e
Corey <coreyortiz130@gmail.com>
2025-08-02 23:47:29 -0400

    SET UP DONE

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include "CPPNotes.h" // Assuming CPPNotes.h contains the declaration of Func()
+//#include "CPPNotes.h" // Assuming CPPNotes.h contains the declaration of Func()
 
 // #include "CPPNotes.cpp" is acceptable, but not recommended due to the potential for multple/redundant definitions.
 // It is better to include the header file CPPNotes.h which contains the function declaration. 
@@ -12,7 +12,8 @@
 
 
 int main (void) {
-    std::cout << basicFunc(); 
-    std::cout << __cplusplus << std::endl;
+    // std::cout << basicFunc(); 
+    // Kido if you want to test functions just uncomment lines from here and CPPNotes.cpp 
+    std::cout << "corey you trolling" << std::endl;
     return 0;
 }
