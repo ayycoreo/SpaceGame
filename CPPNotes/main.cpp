258afee78d6b3c1dbcbb3a73c3cf4cec80924274
Corey <coreyortiz130@gmail.com>
2025-08-06 01:11:24 -0400

    Initializing Framework

---
+++ CPPNotes/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+//#include "CPPNotes.h" // Assuming CPPNotes.h contains the declaration of Func()
+
+
+
+//Here is the main C++ code that will need to be compiled and run to see all the changes in action:
+
+
+int main (void) {
+    // Kido if you want to test functions just uncomment lines from here and CPPNotes.cpp 
+    std::cout << "corey you trolling" << std::endl;
+    return 0;
+}
